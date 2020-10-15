fd5c38ac4a757ad1c6d9884c8d57c1f5809f4842
Henrique <henrique.aztn@gmail.com>
2020-10-15 20:05:17 +0100

    show on scroll

---
+++ static/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    // Smooth scroll to anchor links
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 500);
+        }
+    });
+
+    let observer = new IntersectionObserver( entries => {
+        entries.forEach( entry => {
+            if (entry.isIntersecting) {
+                $(entry.target).addClass("is-visible")
+            } else {
+                $(entry.target).removeClass("is-visible")
+            }
+            
+        })
+        
+    });
+
+    observer.observe(document.querySelector('.fade-in-section'));
+
+});
